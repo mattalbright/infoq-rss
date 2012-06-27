f7e97d190761ff7ef6a58d2574bea30b4f5612e5
Matthew Albright <albright@adobe.com>
2012-06-27 11:23:23 -0700

    Cleaned up some things, and shortened others.

--- app.rb
+++ app.rb
@@ -4,28 +4,33 @@ require 'open-uri'
 class InfoQApp < Sinatra::Base
 
 get '/rss' do
-	token = params[:token] || 'DrROHqmA6qPdIMgWBTVyXnko3xVw5YBZ'
-	rss_out = RSS::Maker.make('2.0') do |maker|
+	# Default is everything... create your own infoq account and exclude stuff if you want in Preferences.
+	token = params[:token] || 'FyBAnM6KAPnHXKa0T2oa0NCGJ6hRjrAU' 
+	rss_out = RSS::Maker.make('2.0') do |feed_out|
 		url = "http://www.infoq.com/rss/rss.action?token=#{token}"
 		open(url) do |rss_in|
-			feed = RSS::Parser.parse(rss_in)
-			maker.channel.title = feed.channel.title
-			maker.channel.link = feed.channel.link
-			maker.channel.description = feed.channel.description
+			feed_in = RSS::Parser.parse(rss_in)
+			feed_out.channel.title = feed_in.channel.title
+			feed_out.channel.link = feed_in.channel.link
+			feed_out.channel.description = feed_in.channel.description
 
-			feed.items.each do |i|
-				next unless i.title.start_with?("Presentation")
+			feed_in.items.each do |item_in|
+				next unless item_in.title.start_with?("Presentation")
 				preso_path = false
-				i.link.match('/presentations/(.+)$') { |m| preso_path = m[1] }
+				item_in.link.match('/presentations/(.+)$') { |m| preso_path = m[1] }
 				next unless preso_path
 				
-				maker.items.new_item do |item|
-					item.link = i.link
-					item.title = i.title
-					item.description = i.description
-					item.enclosure.url = "http://#{env['HTTP_HOST']}/mp3/#{preso_path}?token=#{token}"
-					item.enclosure.length = 65_000_000
-					item.enclosure.type = 'audio/mpeg'
+				feed_out.items.new_item do |item_out|
+					item_out.link = item_in.link
+					item_out.title = item_in.title
+					item_out.description = item_in.description
+					item_out.pubDate = item_in.pubDate
+					item_out.enclosure.url = "http://#{env['HTTP_HOST']}/mp3/#{preso_path}"
+					item_out.enclosure.type = 'audio/mpeg'
+					
+					# From http://www.rssboard.org/rss-profile#element-channel-item-enclosure :
+					# "When an enclosure's size cannot be determined, a publisher SHOULD use a length of 0."
+					item_out.enclosure.length = 0
 				end
 			end
 		end
@@ -35,26 +40,15 @@ get '/rss' do
 	rss_out.to_s
 end
 
-def login(token)
-	form_data = {
-		'DrROHqmA6qPdIMgWBTVyXnko3xVw5YBZ' => {
-			'username' => 'mattalbright@gmail.com',
-			'password' => 'mattalbrightpass',
-		}
-	}[token]
-	
-	return unless form_data
-	
-	curl = Curl::Easy.new("https://www.infoq.com/login.action")
-	params = form_data.keys.map { |k| Curl::PostField.content(k, form_data[k]) }
-	
+def get_user_cookie
 	user_cookie = ""
 	
+	curl = Curl::Easy.new("https://www.infoq.com/login.action")
 	curl.on_header do |header|
 		header.match('RegisteredUserCookie=[^;]+') { |m| user_cookie = m[0] }
 		header.length
 	end
-	curl.http_post(*params)
+	curl.http_post(URI.encode_www_form('username' => 'mattalbright+infoq@gmail.com', 'password' => 'infoqpass'))
 	
 	user_cookie
 end
@@ -82,9 +76,7 @@ def get_mp3_url(preso_path, user_cookie)
 end
 
 get '/mp3/:preso_path' do |preso_path|
-	user_cookie = login(params[:token])
-	mp3_url = get_mp3_url(preso_path, user_cookie)
-	redirect mp3_url
+	redirect get_mp3_url(preso_path, get_user_cookie)
 end
 
 end
